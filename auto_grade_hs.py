b571c4f848b47e510432ba51bdbeaa3eef84f159
LazySheeeeep <mapleontheviolin@proton.me>
2025-01-20 20:02:46 -0800

    init

---
+++ auto_grade_hs.py
@@ -0,0 +1,131 @@
+import os
+import argparse
+import subprocess
+
+ERROR_STR = "*error*"
+
+class GHCI:
+    def __init__(self, file):
+        self.g = subprocess.Popen(
+            ["ghci", file],
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,
+            bufsize=1
+        )
+        self.prompt = "ghci> "
+        if self.flush():
+            self.close()
+            raise Exception(f"{args.hs_file} load error")
+        
+    def read_out(self):
+        error = True
+        while True:
+            line = self.g.stdout.readline()
+            if "Ok, " in line:
+                error = False
+            if self.prompt in line:
+                return error
+
+    def run(self, command, capture_output=True):
+        self.g.stdin.write(f"{command}\n")
+        self.g.stdin.flush()
+        if not capture_output: return
+        output = self.g.stdout.readline()
+        return output.removeprefix(self.prompt).strip()
+    
+    def close(self):
+        self.run(":quit", capture_output="False")
+        self.g.communicate()
+
+    def flush(self):
+        self.run('1+1', capture_output=False)
+        return self.read_out()
+
+    def test(self, commands):
+        results = []
+        for command in commands:
+            output = self.run(command)
+            if output == '':  # error occured
+                self.flush()
+                output = ERROR_STR
+            results.append(output)
+        return results
+
+def load_file(file_path):
+    with open(file_path, 'r') as f:
+        data = f.read()
+    return [section.splitlines() for section in data.strip().split('\n\n')]
+
+def write_answers(answerss, outfile):
+    with open(outfile, 'w') as f:
+        for answers in answerss:
+            for answer in answers:
+                print(answer, file=f)
+            print(file=f)
+
+def prepare_info(ti_file, to_file):
+    info = {}
+    info["test_outputs"] = load_file(to_file)
+    test_inputs = load_file(ti_file)
+    info["max_scores"] = [int(i[0]) for i in test_inputs]
+    info["test_inputs"] = [i[1:] for i in test_inputs]
+    info["num_cases"] = [len(i) for i in info["test_inputs"]]
+    info["num_questions"] = len(info["num_cases"])
+    info["case_credits"] = [info["max_scores"][i] / info["num_cases"][i]
+                           for i in range(info["num_questions"])]
+    return info
+
+def get_answerss(hs_file, test_inputs):
+    ghci = GHCI(hs_file)
+    answerss = [ghci.test(commands) for commands in test_inputs]
+    ghci.close()
+    return answerss
+
+def grade(answerss, info):
+    # scoring
+    score = 0
+    mistakes = {}
+    for i in range(info["num_questions"]): # #Q
+        for j in range(info["num_cases"][i]):
+            if answerss[i][j] == info["test_outputs"][i][j]:
+                score += info["case_credits"][i]
+            else:
+                if i in mistakes:
+                    mistakes[i].append(j)
+                else:
+                    mistakes[i] = [j]
+    
+    # commenting
+    if len(mistakes) != 0:
+        comment = "Failed Test case(s):\n"
+        for i in mistakes:
+            comment += f"Q{i+1}\n"
+            for j in mistakes[i]:
+                comment += f'(-{info["case_credits"][i]}pt) Input: {info["test_inputs"][i][j]}\tExpected: {info["test_outputs"][i][j]}\tYours: {answerss[i][j]}\n'
+    else:
+        comment = None
+    return score, comment
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Auto grading script for .hs files.")
+    parser.add_argument('-hf', '--hs-file', type=str, required=True,
+                        help="Path to the Haskell (.hs) file to be graded.")
+    parser.add_argument('-ti', '--test-inputs', type=str, default="in.txt")
+    parser.add_argument('-to', '--test-outputs', type=str, default="out.txt")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    
+    if not os.path.exists(args.test_outputs):
+        ghci = GHCI(args.hs_file)
+        test_inputs = load_file(args.test_inputs)
+        answerss = [ghci.test(commands) for commands in test_inputs]
+        write_answers(answerss, args.test_outputs)
+    else:
+        info = prepare_info(args.test_inputs, args.test_outputs)
+        answerss = get_answerss(hs_file=args.hs_file, test_inputs=info["test_inputs"])
+        score, comment = grade(answerss, info)
+        print(score, "\n", comment)
---
+++ batch_grade.py
@@ -0,0 +1,77 @@
+import os
+import glob
+import argparse
+import csv
+import json
+from auto_grade_hs import prepare_info, get_answerss, grade
+
+SFUID_COL = "SIS User ID"
+CMPTID_COL = "SIS Login ID"
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Batch grading script for Haskell (.hs) files.")
+    parser.add_argument('-sd', '--submission-directory', type=str, required=True,
+                        help="(e.g., './H1_submissions/').")
+    parser.add_argument('-ti', '--test-inputs', type=str, default="in.txt")
+    parser.add_argument('-to', '--test-outputs', type=str, default="out.txt")
+    parser.add_argument('-sf', '--score-file', type=str, default="../Grades-CMPT383_D100.csv")
+    parser.add_argument('-cf', '--comments-file', type=str, default="comments.json")
+    parser.add_argument('-cn', '--column-name', type=str, required=True)
+    return parser.parse_args()
+
+def load_csv(filename):
+    grades = {}
+
+    with open(filename, mode="r", encoding="utf-8") as csv_file:
+        reader = csv.DictReader(csv_file)
+        fieldnames = reader.fieldnames
+        
+        rows = list(reader)
+
+        for row in rows:
+            sfuid = row.get(SFUID_COL, "")
+            if sfuid:
+                grades[sfuid] = row
+            else:
+                ...
+    return grades, fieldnames
+
+def save_csv(output_file, fieldnames, content_dict):
+    with open(output_file, mode="w", encoding="utf-8", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in content_dict.values():
+            writer.writerow(row)
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    auto_grade_script = "../auto_grade_hs.py"
+    comments = {}
+    scores, fieldnames = load_csv(args.score_file)
+    info = prepare_info(args.test_inputs, args.test_outputs)
+    
+    for hs_file in glob.glob(os.path.join(args.submission_directory, "*.hs")):
+        try:
+            basename = os.path.basename(hs_file)
+            sfuid = basename[-12:-3]
+            if "-" in sfuid:
+                sfuid = basename[-14:-5]
+            if not sfuid.isdigit():
+                raise Exception(f"cannot extract sfuid from {basename}.")
+            elif sfuid not in scores:
+                raise Exception(f"sfuid {sfuid} does not exist.")
+            answerss = get_answerss(hs_file=hs_file, test_inputs=info["test_inputs"])
+            score, comment = grade(answerss, info)
+            scores[sfuid][args.column_name] = score
+            if comment is not None:
+                comments[scores[sfuid][CMPTID_COL]] = comment
+
+            print(f"Graded {sfuid}", end='\r')
+
+        except Exception as e:
+            print(f"Failed to grade {hs_file}: {e}")
+
+    save_csv("scores.csv", fieldnames, scores) # args.score_file
+    with open(args.comments_file, "w") as file:
+        json.dump(comments, file, indent=4)
