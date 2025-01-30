c5a1ce5c6061e31aa355438a30fa80762baa2088
LazySheeeeep <mapleontheviolin@proton.me>
2025-01-29 21:24:42 -0800

    feat: adding local llm as an assistant

--- batch_grade.py
+++ batch_grade.py
@@ -23,17 +23,40 @@ def grade_(hs_file):
         scores[userid] = score
         if comment is not None:
             comments[userid] = comment
+        if assistance and check(hs_file):
+            double_check_files.append(hs_file)
     except Exception as e:
         print(f"Failed to grade {hs_file}: {e}")
+
+def check(hs_file: str) -> bool:
+    with open(hs_file, 'r') as f:
+            script_content = f.read()
+    response = ollama.chat(model="qwen2.5-coder-32b-instruct-q4_k_m",
+                           messages=[{"role": "user", "content": prompt + script_content}],
+                           options={"temperature":0})
+    return "0" in response.message.content  # True if sth is wrong
 
 if __name__ == "__main__":
     args = parse_arguments()
     info = prepare_info(args.test_inputs, args.test_outputs)
     comments = {}
     scores = {}
+    assistance = False
+    double_check_files = []
+
+    if os.path.exists("prompt.txt"):
+        assistance = True
+        with open("prompt.txt", 'r') as f:
+            prompt = f.read()
+        import ollama
+    
     for hs_file in tqdm(glob.glob(os.path.join(args.submission_directory, "*.hs"))):
         grade_(hs_file)
-        
+
+    if len(double_check_files) > 0:
+        print("Double-check the following files:")
+        for f in double_check_files:
+            print(f)
     
     with open(args.scores_file, "w") as file:
         json.dump(scores, file, indent=4)
